f6274a86bd5ce375a23c199a48b60c392aacded5
Jeremy Collette <jeremy.collette93@gmail.com>
2020-09-05 15:46:48 -0700

    Now using binary to reduce load on serial port

--- src/SerialLedCommandInput.cpp
+++ src/SerialLedCommandInput.cpp
@@ -23,6 +23,61 @@ bool SerialLedCommandInput::Initialize()
 bool SerialLedCommandInput::TryGetNextCommand(LedCommand* command_out)
 {
     // TODO(jeremy): move this logic to tick
+    auto available_bytes = Serial.available();
+    if (available_bytes < 3)
+    {
+        return false;
+    }
+
+    char printBuf[50];
+    snprintf(printBuf, 50, "Available bytes: %d", available_bytes);
+    logger_.Log(SmartPiano::TEST, printBuf);
+
+    char check_byte = Serial.read();
+    if (check_byte != 0x32)
+    {
+        snprintf(printBuf, 50, "Got invalid check byte: %d", check_byte);
+        logger_.Log(SmartPiano::WARNING, printBuf);
+        return false;
+    }
+
+    int key = Serial.read();
+    int on = Serial.read();
+
+    snprintf(printBuf, 50, "Got command: %d %d", key, on);
+    logger_.Log(SmartPiano::TEST, printBuf);
+
+    command_out->index = key;
+    if (!on)
+    {
+        command_out->red = 0;
+        command_out->green = 0;
+        command_out->blue = 0;
+        command_out->white = 0;
+        return true;
+    }
+
+    if (key >= 60)
+    {
+        command_out->red = 255;
+        command_out->green = 0;
+        command_out->blue = 0;
+        command_out->white = 0;
+    }
+    else
+    {
+        command_out->red = 0;
+        command_out->green = 0;
+        command_out->blue = 255;
+        command_out->white = 0;
+    }
+    return true;
+}
+
+/*
+bool SerialLedCommandInput::TryGetNextCommand(LedCommand* command_out)
+{
+    // TODO(jeremy): move this logic to tick
     //logger_.Log(LogLevel::TEST, "Test!");
     if (!Serial.available())
     {
@@ -71,6 +126,7 @@ bool SerialLedCommandInput::TryGetNextCommand(LedCommand* command_out)
     }
     return true;
 }
+*/
 
 void SerialLedCommandInput::Tick(int delta)
 {
